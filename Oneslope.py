970cae65dcf46a2eaed4fcc21836d34063620adb
nake12 <nake37@gmail.com>
2015-09-17 15:16:12 -0400

    add Oneslope

---
+++ Oneslope.py
@@ -0,0 +1,14 @@
+__author__ = 'nake12'
+
+users2 ={
+"Amy": {"Taylor Swift": 4, "PSY": 3, "Whitney Houston": 4},
+"Ben": {"Taylor Swift": 5, "PSY": 2},
+"Clara": {"PSY": 3.5, "Whitney Houston": 4},
+"Daisy": {"Taylor Swift": 5, "Whitney Houston": 3}
+}
+
+def computeDeviations(Ratings):
+
+
+def OneslopeRecommander(Ratings):
+
