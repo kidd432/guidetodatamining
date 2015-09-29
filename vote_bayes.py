3863d38915ea38b3205d9e484e8897097357b6d3
nake12 <nake37@gmail.com>
2015-09-28 21:50:12 -0400

    Add bayes method on vote example

---
+++ vote_bayes.py
@@ -0,0 +1,7 @@
+__author__ = 'nakehttp://pan.baidu.com/s/1gdo9x1x12'
+
+class voteClassify:
+    def __init__(self,filename):
+        return 0
+    def classify(self,item,number):
+        return 0
